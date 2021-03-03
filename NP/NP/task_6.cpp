e6677dacf00b10e5bf71f292b4bb6fe50a79046b
olhagrubryn <78905756+olhagrubryn@users.noreply.github.com>
2021-03-03 20:36:38 +0200

    Add files via upload

---
+++ NP/NP/task_6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<stdio.h>
+#include<windows.h>
+
+using namespace std;
+
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int m, n, a;
+	cout << "Введіть кількіть останніх цифр:" << endl;
+	cin >> m;
+	cout << "Ведіть саме число(натуральне): " << endl;
+	cin >> n;
+	if (m > 0 && n > 0) {
+		for (int i = 1, sum = 0; i <= m; i++) {
+			a = n % 10;
+			sum += a;
+			n /= 10;
+
+
+		}
+	}
+	else {
+		cout << "Виввели від'ємне число!" << endl;
+	}
+	cout << "Сума " << m << " останніх елементів дорівнює " << (int)sum << endl;
+
+	system("pause");
+}
---
+++ NP/NP/task_7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<stdio.h>
+#include<windows.h>
+
+using namespace std;
+
+int main() {
+
+	system("pause");
+}
